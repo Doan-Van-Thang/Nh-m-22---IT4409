b7a6c9069b71179124ca733af1a4480d757f5cd7
henrivu1285 <minecraftteam125@gmail.com>
2025-10-26 14:39:38 +0700

    Refactor game structure: update player, websocket , bullet in server

--- server/src/model/Bullet.js
+++ server/src/model/Bullet.js
@@ -1,19 +1,24 @@
 export default class Bullet {
-    constructor(id,x,y,rotation,playerId){
+    constructor(id,x,y,rotation,playerId,speed = 5){
         this.id = id;
         this.x =x;
         this.y=y;
         this.rotation = rotation;
         this.playerId = playerId;
         this.time = Date.now();
+        this.speed = speed;
     }
 
     update(){ //Update viên đạn theo hướng
-        this.x += Math.cos(this.rotation)*3;
-        this.y += Math.sin(this.rotation)*3;
+        this.x += Math.cos(this.rotation)*thí.speed;
+        this.y += Math.sin(this.rotation)*this.speed;
     }
 
     isExpired(){ //kiểm tra thời gian tồn tại của đạn
         return Date.now() - this.time > 4000;
     }
+
+    isOutofBounds(mapWidth,mapHeight){ //kiểm tra đạn có ra ngoài bản đồ không
+        return this.x <0 || this.x > mapWidth || this.y <0 || this.y > mapHeight;
+}
 }
