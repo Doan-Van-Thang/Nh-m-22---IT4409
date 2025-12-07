3b1bf422dc1878111f0c455164fc58b63a7208fd
Doan-Van-Thang <doanthang250604@gmail.com>
2025-12-07 20:31:24 +0700

    add file

--- tank_online/src/main.jsx
+++ tank_online/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import "./styles/game.css"; // nếu có stylesheet
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+//npm create vite@latest my-app -- --template react
